35155ea5174c8bed1498a1c71bce2b0db3eacf6a
wayne tsao <a578ff6@gmail.com>
2023-10-02 16:14:18 +0800

    重新上傳

--- Create Programmatic Segues/ViewController.swift
+++ Create Programmatic Segues/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
+
+
+// MARK: - 使用performSegue
 class ViewController: UIViewController {
 
+    // 開關：將決定是否允許跳轉
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // 當綠色按鈕被點擊時呼叫的方法
+    @IBAction func greenButtonTapped(_ sender: UIButton) {
+        // 檢查開關是否為打開狀態
+        if segueSwitch.isOn {
+            // 如果開關是打開的，進行帶有指定id的跳轉
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
+    // 當黃色按鈕被點擊時呼叫的方法
+    @IBAction func yellowButtonTapped(_ sender: UIButton) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
 }
+
+
 
